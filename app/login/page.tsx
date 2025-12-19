83da3a105b95ef673585798b7c807d924c9a06be
azizhadiid <azizalhadiid88@gmail.com>
2025-12-19 14:18:05 +0700

    Solve Layout is Done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,7 @@ export default function AdminLogin() {
                         <div className="mx-auto w-16 h-16 bg-linear-to-br from-orange-500 to-orange-600 rounded-2xl flex items-center justify-center shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300">
                             <Lock className="w-8 h-8 text-white" />
                         </div>
-                        <CardTitle className="text-3xl font-bold bg-linear-to-r from-slate-700 to-slate-900 bg-clip-text text-transparent">
+                        <CardTitle className="text-3xl bg-linear-to-r from-slate-700 to-slate-900 bg-clip-text text-transparent">
                             Admin Portal
                         </CardTitle>
                         <CardDescription className="text-base text-slate-600">
