d5db3f8f4ed8f778612f2c59e161bdd77320f7a4
azizhadiid <azizalhadiid88@gmail.com>
2025-12-25 15:14:20 +0700

    Start Dashboard Admin

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import AdminDashboard from '@/components/pages/admin/DashboardAdminPage';
+import type { Metadata } from 'next';
+
+// 'metadata' sekarang berada di Server Component, ini sudah benar.
+export const metadata: Metadata = {
+    title: 'Dashbaord Admin', // Ini akan mengisi placeholder %s
+};
+
+// Ini adalah Server Component (tanpa "use client")
+export default function LoginAdminPage() {
+    // Tugasnya hanya me-render Client Component
+    return <AdminDashboard />;
+}
